3fc1369cb60d14902d8407ad25507a7477a052d6
Tejal Bagade <tejal.bgd@gmail.com>
2023-05-17 16:42:29 -0400

    3D objects
    Adding Torus knot, sphere and plane for visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-
 export default function App(){
     return <>
-                <mesh>
-                    <torusKnotBufferGeometry/>
-                    <meshNormalMaterial/>
+                <mesh position-x={-2}>
+                    <sphereGeometry />
+                    <meshNormalMaterial color='red' wireframe/>
+                </mesh>
+                <mesh position={[2,Math.PI*0.25,0]} scale={1.5}>
+                    <boxGeometry scale={1.5}/>
+                    <meshNormalMaterial color='red' wireframe/>
+                </mesh>
+                <mesh position-x={2} position-y={Math.PI*0.25} scale={1}>
+                    <planeGeometry />
+                    <meshBasicMaterial color='green' wireframe/>
                 </mesh>
             </>
 }
